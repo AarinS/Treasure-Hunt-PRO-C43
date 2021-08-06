1d91ebb537f001324197eeb0e59286be758e3988
AarinS <77874039+AarinS@users.noreply.github.com>
2021-08-07 09:41:55 +1200

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,24 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("O F I N T U C N",700,150)
+    fill("lightblue")
+    text("Hint: Perfroms a specific task !!", 650,170)
+    fill("white")
+    textSize(15)
+    text("ASBATADE", 100,280)
+    fill("lightblue")
+    text("Hint: Stores informaion !!", 100,295)
+
+}
